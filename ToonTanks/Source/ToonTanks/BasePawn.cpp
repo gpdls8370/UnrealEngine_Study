38f829af46cb18b8c9a1535805dcea46d3aac605
박혜인 <ms8370@naver.com>
2023-11-05 02:38:21 +0900

    베이스 폰, 탱크 생성

---
+++ ToonTanks/Source/ToonTanks/BasePawn.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BasePawn.h"
+#include "Components/CapsuleComponent.h"
+
+// Sets default values
+ABasePawn::ABasePawn()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	CapsuleComp = CreateDefaultSubobject<UCapsuleComponent>("Capsule Collider");
+	RootComponent = CapsuleComp;
+
+	BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>("Base Mesh");
+	BaseMesh->SetupAttachment(CapsuleComp);
+
+	TurretMesh = CreateDefaultSubobject<UStaticMeshComponent>("Turret Mesh");
+	TurretMesh->SetupAttachment(BaseMesh);
+
+	ProjectileSpawnPoint = CreateDefaultSubobject<USceneComponent>("Spawn Point");
+	ProjectileSpawnPoint->SetupAttachment(TurretMesh);
+}
+
+// Called when the game starts or when spawned
+void ABasePawn::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+// Called every frame
+void ABasePawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ ToonTanks/Source/ToonTanks/Tank.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Tank.h"
+#include <GameFramework/SpringArmComponent.h>
+#include <Camera/CameraComponent.h>
+
+ATank::ATank()
+{
+	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>("Spring Arm");
+	SpringArmComp->SetupAttachment(RootComponent);
+
+	CameraComp = CreateDefaultSubobject<UCameraComponent>("Camera");
+	CameraComp->SetupAttachment(SpringArmComp);
+}
+
+// Called to bind functionality to input
+void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &ATank::Move);
+}
+
+void ATank::Move(float Value)
+{
+
+}
+
